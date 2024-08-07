2efd4020e6d7f611719d597d170e9a0498f9218a
Szymon Ból <bol.szymon@gmail.com>
2024-08-08 01:37:29 +0200

    get / post / patch API requests

--- backend-wgc/src/index.js
+++ backend-wgc/src/index.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { getMongodbClient } from './connection.js';
 import express from 'express';
-import { findInvitationById } from './wedding-crud.js'
+import { createInvitation, findInvitationById, updateIsGoing } from './wedding-crud.js'
 
 config();
 
@@ -13,12 +13,29 @@ const mongoClient = await getMongodbClient().connect();
 const db = mongoClient.db(dbName);
 const guestCollection = db.collection(collectionName);
 const app = express();
-
+app.use(express.json());
 
 app.get('/invitation/:id', async (req, res) => {
     const id = req.params.id;
     const reulst = await findInvitationById(guestCollection, id);
     res.send(reulst);
+})
+
+app.post('/create-invitation', async (req, res) => {
+  const invitation = req.body;  
+  const reulst = await createInvitation(guestCollection, invitation);
+  res.send(reulst);
+})
+
+// {
+// 	   "id": "wercia_jawor_test",
+//     "name": "Weronika",
+//     "isGoing": "yes"
+// }
+app.patch('/update-is-going', async (req, res) => {
+  const {id, name, isGoing}= req.body;  
+  const reulst = await updateIsGoing(guestCollection, id, name, isGoing);
+  res.send(reulst);
 })
 
 app.listen(port, () => {
--- backend-wgc/src/wedding-crud.js
+++ backend-wgc/src/wedding-crud.js
@@ -1,3 +1,15 @@
 export async function findInvitationById(collection, id) {
     return collection.find({ id }).toArray();
  }
+
+ export async function createInvitation(collection, invitationObj) {
+    await collection.insertOne(invitationObj);
+ }
+
+ export async function updateIsGoing(collection, idVal, guestName, isGoing) {
+    await collection.updateOne(
+        { id : idVal},
+        { $set: { "values.$[elem].isGoing" : isGoing } },
+        { arrayFilters: [ { "elem.name": guestName } ]}
+    );
+ }
