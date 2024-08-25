11f5525fd6dc47839570f1c5a11b7b780c1f992c
Szymon Ból <bol.szymon@gmail.com>
2024-08-26 00:57:25 +0200

    invite confirm preparation

--- frontend-wgc/src/app/guest-panel/invite-confirmation/invite-confirmation.component.ts
+++ frontend-wgc/src/app/guest-panel/invite-confirmation/invite-confirmation.component.ts
@@ -2,25 +2,40 @@ import { Component, inject } from '@angular/core';
 import { GuestDataStore } from '../../shared/store/guest-panel.store';
 import { JsonPipe } from '@angular/common'
 import { ActivatedRoute } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { DietSwitchComponent } from "../../shared/components/diet-switch/diet-switch.component";
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-invite-confirmation',
   standalone: true,
-  imports: [JsonPipe, MatIconModule, DietSwitchComponent],
+  imports: [JsonPipe, MatIconModule, DietSwitchComponent, MatButtonModule, ReactiveFormsModule, FormsModule, MatTableModule],
   templateUrl: './invite-confirmation.component.html',
   styleUrl: './invite-confirmation.component.scss'
 })
 export class InviteConfirmationComponent {
-    store = inject(GuestDataStore);
-    private activateRoute= inject(ActivatedRoute);
+  store = inject(GuestDataStore);
+  private activateRoute = inject(ActivatedRoute);
 
-    guestsSig = this.store.guestsData!;
 
-    constructor(){
-      const id= this.activateRoute.snapshot.queryParamMap.get('id');
-      if(id)
-        this.store.fetchInvitationDataById(id);
+  guestsSig = this.store.guestsData!;
+  displayedColumns: string[] = ['isGoing', 'nameAndSurname', 'diet'];
+
+  form = new FormGroup(
+    {
+      isVege: new FormControl(false, [Validators.required, Validators.requiredTrue])
     }
+  )
+
+  constructor() {
+    const id = this.activateRoute.snapshot.queryParamMap.get('id');
+    if (id)
+      this.store.fetchInvitationDataById(id);
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
--- frontend-wgc/src/app/shared/components/diet-switch/diet-switch.component.ts
+++ frontend-wgc/src/app/shared/components/diet-switch/diet-switch.component.ts
@@ -1,21 +1,44 @@
-import { Component, signal } from '@angular/core';
+import { Component, forwardRef, model, signal } from '@angular/core';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import { NgClass } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const NG_DIET_COMPONENT_VALUE_ACCESSOR = {       
+  provide : NG_VALUE_ACCESSOR, 
+  useExisting: forwardRef(() => DietSwitchComponent),
+  multi: true     
+} 
 
 @Component({
   selector: 'app-diet-switch',
   standalone: true,
   imports: [MatSlideToggleModule, MatIconModule, NgClass],
   templateUrl: './diet-switch.component.html',
-  styleUrl: './diet-switch.component.scss'
+  styleUrl: './diet-switch.component.scss',
+  providers : [NG_DIET_COMPONENT_VALUE_ACCESSOR]
 })
-export class DietSwitchComponent {
+export class DietSwitchComponent implements ControlValueAccessor{
+  
+  isVege = signal<boolean>(false);
 
-  isVege =signal<boolean>(false);
+  onChange: any = () => {}
+  onTouch: any = () => {}
 
-  changeValue() {
-    this.isVege.update(val => !val);
+  writeValue(val: boolean): void {
+    this.isVege.set(val)
   }
 
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn
+  }
+
+  changeValue() {
+    this.writeValue(!this.isVege());
+    this.onChange(this.isVege());
+  }
 }
