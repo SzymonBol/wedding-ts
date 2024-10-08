f0fec8cd779960f27e186ec3a01e9b1553f61c1a
Szymon Ból <bol.szymon@gmail.com>
2024-10-09 01:46:12 +0200

    add map with OSADA coordinates

--- frontend-wgc/src/app/guest-panel/additional-info/additional-info.component.ts
+++ frontend-wgc/src/app/guest-panel/additional-info/additional-info.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule, MapMarker } from "@angular/google-maps";
 
 @Component({
   selector: 'app-additional-info',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './additional-info.component.html',
   styleUrl: './additional-info.component.scss'
 })
 export class AdditionalInfoComponent {
+  options: google.maps.MapOptions = {
+    mapId: "DEMO_MAP_ID",
+    center: { lat: 49.55224231685368, lng: 20.37067956832379 },
+    zoom: 12,
+  };
 
+  marker = { lat: 49.55224231685368, lng: 20.37067956832379 }
 }
