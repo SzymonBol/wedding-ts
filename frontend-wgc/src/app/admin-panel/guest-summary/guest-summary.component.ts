6db14be9b083b8e46d12382db822d6d108df73db
Szymon Ból <bol.szymon@gmail.com>
2024-12-12 01:46:00 +0100

    sumarry data

--- frontend-wgc/src/app/admin-panel/guest-summary/guest-summary.component.ts
+++ frontend-wgc/src/app/admin-panel/guest-summary/guest-summary.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AdminStore } from '../../shared/store/admin.store';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-guest-summary',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './guest-summary.component.html',
   styleUrl: './guest-summary.component.scss'
 })
 export class GuestSummaryComponent {
+  private invitations = inject(AdminStore).entities;
 
+  summaryData = computed(() => {
+    const invitationsData = this.invitations();
+    let confirmedCount = 0;
+    let vegeCount = 0;
+    let meatCount = 0;
+    let goingGuest = 0;
+    let refusedGuest = 0;
+    let notConfirmedGuestsCount = 0;
+    let needAccommodationCount =0;
+    let allGuestsCount = 0;
+
+    if(invitationsData){
+      invitationsData.forEach(invitation => {
+        if(invitation.confirmed){
+          invitation.guests.forEach(guest => {
+            if(guest.isGoing) {
+              goingGuest++;
+              if(guest.isVege) vegeCount++; else meatCount ++;
+              
+              if(invitation.needAccommodation) needAccommodationCount++;
+            } else refusedGuest++;
+            
+            confirmedCount++;
+            allGuestsCount++;
+          })
+        } else {
+          invitation.guests.forEach(() => {
+            notConfirmedGuestsCount++;
+            allGuestsCount++;
+          })
+        }
+      })
+    }
+
+    return {
+      confirmedCount,
+      notConfirmedGuestsCount,
+      goingGuest,
+      refusedGuest,
+      meatCount,
+      vegeCount,
+      needAccommodationCount,
+      allGuestsCount
+    }
+  })
 }
